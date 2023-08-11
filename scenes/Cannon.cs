ea8c3b8df5734f484c0b5adf40725cc7c7ad98ae
unfinishedideas <unfinishedideas@gmail.com>
2023-08-10 21:16:33 -0700

    added more UI to show loading cannons  faster

--- scenes/Cannon.cs
+++ scenes/Cannon.cs
@@ -6,21 +6,30 @@ public partial class Cannon : Area2D
 	AnimatedSprite2D BarrelAnimator;
 	AnimatedSprite2D StatusAnimator;
 	AnimatedSprite2D LoadBarAnimator;
+
 	enum CannonState {
 		empty,
 		loading,
-		ready
+		ready,
+		cooldown
 	}
-	private CannonState state = CannonState.ready;
+	private CannonState state = CannonState.empty;
+	private bool has_fired = false;
+	private bool ready_to_fire  = false;
+
 	private bool in_interactable_range = false;
 	private bool being_interacted = false;
-	private float loading_status = 0.0f;
-	int num_loadbar_frames = 1;
 
+	private float loading_status = 0.0f;
+	private int num_loadbar_frames = 1;
+	private int num_coolbar_frames = 1;
+
+	
+	// Mess with these in the editor to tweak behavior!
 	[Export] public float loading_speed = 0.001f;
 	[Export] public float interaction_speed_bonus = 0.003f;
-	[Export] public bool flip_status = false;
-
+	[Export] public float cooldown_speed = 0.002f;
+	[Export] public bool flip_status_icons = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -28,56 +37,96 @@ public partial class Cannon : Area2D
 		BarrelAnimator = GetNode<AnimatedSprite2D>("BarrelAnimator");
 		StatusAnimator = GetNode<AnimatedSprite2D>("StatusAnimator");
 		LoadBarAnimator = GetNode<AnimatedSprite2D>("LoadBarAnimator");
-		if (flip_status == true)
-		{
+		if (flip_status_icons == true) {
 			StatusAnimator.RotationDegrees = 180;
 			LoadBarAnimator.RotationDegrees = 180;
 		}
-
+		StatusAnimator.Animation = "loading";
 		StatusAnimator.Play();
-		num_loadbar_frames = LoadBarAnimator.SpriteFrames.GetFrameCount("default");
+		num_loadbar_frames = LoadBarAnimator.SpriteFrames.GetFrameCount("loading");
+		num_coolbar_frames = LoadBarAnimator.SpriteFrames.GetFrameCount("cooldown");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (state == CannonState.loading && in_interactable_range == true)
-		{
-			if (loading_status >= 1.0f)
-			{
-				loading_status = 0;
-				SetCannonState(CannonState.ready);
-			}
-			else 
-			{
-				loading_status += loading_speed;
-				if (being_interacted)
-				{
-					loading_status += interaction_speed_bonus;
-				}
-				int frame = (int)Math.Floor(num_loadbar_frames * loading_status);
-				LoadBarAnimator.SetFrameAndProgress(frame, 0);
-			}
+		// Loading The Cannon
+		if (state == CannonState.loading && in_interactable_range == true){
+			LoadCannon();
+		}
+		// Cooldown
+		else if (state == CannonState.cooldown && has_fired == true){
+			CooldownCannon();
 		}
 	}
 
+	// Cannon Loading / Cooldown ---------------------------------------------------------------------------------------
+	private void LoadCannon()
+	{
+		if (loading_status >= 1.0f) {
+			loading_status = 1.0f;
+			SetCannonState(CannonState.ready);
+		}
+		else {
+			loading_status += loading_speed;
+			if (being_interacted) {
+				StatusAnimator.SpeedScale = 4.0f;
+				loading_status += interaction_speed_bonus;
+			}
+			else
+			{
+				StatusAnimator.SpeedScale = 1.0f;
+			}
+			LoadBarAnimator.SetFrameAndProgress(
+				GetBarFrameByPercentage(num_loadbar_frames, loading_status),
+				0
+			);
+		}
+	}
+
+	private void CooldownCannon()
+	{
+		loading_status -= cooldown_speed;
+		LoadBarAnimator.SetFrameAndProgress(
+			GetBarFrameByPercentage(num_coolbar_frames, loading_status), 
+			0
+		);
+		if (loading_status <= 0) {
+			has_fired = false;
+			loading_status = 0.0f;
+			SetCannonState(CannonState.empty);
+		}
+	}
+
+	private int GetBarFrameByPercentage(int num_frames, float status)
+	{
+		return (int)Math.Floor(num_frames * status);
+	}
+
+	// Interactions ----------------------------------------------------------------------------------------------------
 	private void InteractHandler()
 	{
-		if (in_interactable_range)
-		{
+		if (in_interactable_range) {
 			being_interacted = true;
 			switch(state)
 			{
 				case CannonState.empty:
 					SetCannonState(CannonState.loading);
+					LoadBarAnimator.Animation = "loading";
 					break;
 
 				case CannonState.loading:
-					SetCannonState(CannonState.loading);
 					break;
 
 				case CannonState.ready:
-					FireCannon();
+					if (ready_to_fire == true)
+					{
+						FireCannon();
+					}
+					break;
+				
+				case CannonState.cooldown:
+					LoadBarAnimator.Animation = "cooldown";
 					break;
 
 				default:
@@ -88,29 +137,33 @@ public partial class Cannon : Area2D
 
 	private void FireCannon()
 	{
-		state = CannonState.empty;
+		has_fired = true;
 		BarrelAnimator.Visible = true;
 		BarrelAnimator.Animation = "fire";
 		BarrelAnimator.Play();
+		SetCannonState(CannonState.cooldown);
 	}
 
 	private void SetCannonState(CannonState new_state)
 	{
-		if (state != new_state)
-		{
+		if (state != new_state) {
 			state = new_state;
 			switch(new_state)
 			{
 				case CannonState.empty:
-					StatusAnimator.Animation = "empty";
+					LoadBarAnimator.Visible = false;
+					StatusAnimator.Visible = false;
 					break;
 				case CannonState.loading:
-					StatusAnimator.Animation = "loading";
 					LoadBarAnimator.Visible = true;
+					StatusAnimator.Visible = true;
 					break;
 				case CannonState.ready:
-					StatusAnimator.Animation = "ready";
-					LoadBarAnimator.Visible = false;
+					LoadBarAnimator.Animation = "ready";
+					StatusAnimator.Visible = false;
+					break;
+				case CannonState.cooldown:
+					LoadBarAnimator.Animation = "cooldown";
 					break;
 				default:
 					break;
@@ -121,17 +174,23 @@ public partial class Cannon : Area2D
 	// Signals ---------------------------------------------------------------------------------------------------------
 	private void _on_body_entered(Node2D body)
 	{
-		if (body.IsInGroup("Player"))
-		{
+		if (body.IsInGroup("Player")) {
 			in_interactable_range = true;
+			if (state == CannonState.loading)
+			{
+				StatusAnimator.Play();
+			}
 		}
 	}
 
 	private void _on_body_exited(Node2D body)
 	{
-		if (body.IsInGroup("Player"))
-		{
+		if (body.IsInGroup("Player")) {
 			in_interactable_range = false;
+			if (state == CannonState.loading)
+			{
+				StatusAnimator.Stop();
+			}
 		}
 	}
 
@@ -147,5 +206,8 @@ public partial class Cannon : Area2D
 	private void _on_player_player_stopped_interacting()
 	{
 		being_interacted = false;
+		if (state == CannonState.ready) {
+			ready_to_fire = true;
+		}
 	}
 }
